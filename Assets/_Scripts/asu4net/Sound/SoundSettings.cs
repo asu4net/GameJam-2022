d67fa4c08a3007412e7fd36f5cad57bbcd0b508d
Alejandro Suárez <asu4net@gmail.com>
2022-09-22 15:12:37 +0200

    Add general to sound settings

--- Assets/_Scripts/asu4net/Sound/SoundSettings.cs
+++ Assets/_Scripts/asu4net/Sound/SoundSettings.cs
@@ -15,12 +15,21 @@ namespace asu4net.Sound
             [Range(0, 1)] public float volume;
         }
 
+        [field: SerializeField]
+        [field: Range(0, 1)] public float general { get; private set; } = 1;
         [field: SerializeField] private List<CategoryVolume> categoryVolumes { get; set; } = new();
         
         public Dictionary<Category, float> volumes { get; private set; } = new();
 
+        public void SetGeneral(float amount)
+        {
+            general = amount;
+            AudioListener.volume = general;
+        }
+        
         public void Initialise()
         {
+            SetGeneral(general);
             foreach (var o in categoryVolumes) volumes.Add(o.category, o.volume);
         }
     }
